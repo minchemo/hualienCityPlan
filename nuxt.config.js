eba1528774da3a10e9c3fe51493abdf3b9994d00
unknown <lalab996@hotmail.com.tw>
2021-10-16 20:03:03 +0800

    fix: ci

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
         // https://go.nuxtjs.dev/eslint
-   '@nuxtjs/device',
+        '@nuxtjs/device',
     ],
 
     // Modules: https://go.nuxtjs.dev/config-modules
