067e3349becd554c19e50bd7fac12730fcdebe04
J莫 <lalab996@hotmail.com.tw>
2021-10-22 15:53:37 +0800

    about

--- pages/about.vue
+++ pages/about.vue
@@ -17,7 +17,7 @@
           </div>
         </div>
       </div>
-      <div class="text">
+      <div class="text" v-bind:class="{ show: intro_text_show }">
         花蓮是個長鏡頭，緩緩悠悠低語慢生，隨著海面東昇的天空，保持穩穩的步調，這是花蓮人的節奏感。人口數量適中的城市，建造在生活的肌理上，產業的樣貌，一方面因著天然資源，一方面跟著日活所需，歷長時交流而成，從原住民、在地人、移居者到新住民，花蓮在文化上亦羅列著歷時融合的光譜，在平靜之中透染著奇幻，在海與脈之間，在地鳴與颱嘯之間，以其自然而生的博物姿態，成為台灣人心中的秘境，一來到花蓮便能因其悠緩而融化。<br /><br />
 
         身為歸鄉的遊子，我有許多遺落的記憶，這座城市有著熟悉的樣貌，而人事早已翻輪，久居的街坊已然進化，回到花蓮，對凡事都感到好奇。花蓮，一直以來也都是富於藝術心靈者嚮往的棲居地，感受花蓮的溫緩，一次次探問而留下體悟成為作品。任何一座城市，都會需要這樣細緻提問，把所思所想讓彼此聽見，在長鏡頭前聚焦。<br /><br />
@@ -34,6 +34,15 @@
 
         每個城市，都應該有這樣的提問。
       </div>
+      <div
+        class="text-more"
+        v-show="$device.isMobile"
+        v-bind:class="{ hide: intro_text_show }"
+        @click="intro_text_show = true"
+      >
+        <span>展開全文</span>
+        <img src="@/assets/img/about/text_more_mo.svg" alt="" srcset="" />
+      </div>
     </div>
     <div class="team">
       <div class="avatar">
@@ -44,8 +53,8 @@
           <div :key="i" class="info-item">
             <div class="title">{{ team.title }}</div>
             <div class="in">
-              <div class="tag">{{ team.tag }}</div>
-              <div class="name" v-if="team.name">{{ team.name }}</div>
+              <div class="tag" v-show="!$device.isMobile">{{ team.tag }}</div>
+              <div class="name" v-show="team.name">{{ team.name }}</div>
               <div class="text" v-html="team.content"></div>
             </div>
           </div>
@@ -54,6 +63,7 @@
     </div>
     <div class="shop" data-aos="fade">
       <img
+        v-if="!$device.isMobile"
         class="title"
         src="@/assets/img/about/shop_title.svg"
         alt=""
@@ -61,8 +71,25 @@
         v-lazy-load
       />
       <img
+        v-else
+        class="title"
+        src="@/assets/img/about/shop_title_mo.svg"
+        alt=""
+        srcset=""
+        v-lazy-load
+      />
+      <img
+        v-if="!$device.isMobile"
         class="logo"
         src="@/assets/img/about/shop_logos.jpg"
+        alt=""
+        srcset=""
+        v-lazy-load
+      />
+      <img
+        v-else
+        class="logo"
+        src="@/assets/img/about/shop_logos_mo.jpg"
         alt=""
         srcset=""
         v-lazy-load
@@ -172,6 +199,7 @@
       margin: 1vw;
       img {
         width: 100%;
+        border: 1px solid #262626;
       }
     }
     .info {
@@ -241,17 +269,193 @@
     }
   }
 }
+
+@media screen and (max-width: 500px) {
+  .main {
+    height: auto;
+    margin-top: $mo_navbar_height;
+
+    .content {
+      width: 100vw;
+      margin: 10% auto;
+      flex-direction: column;
+
+      .intro {
+        position: relative;
+        height: auto;
+        width: 80vw;
+        flex-direction: row;
+        align-items: flex-start;
+        justify-content: space-between;
+        height: 30vw;
+        padding: 0;
+        left: 10vw;
+        margin: 10vw 0;
+        h2 {
+          font-size: 19pt;
+          margin-top: 0;
+          margin-bottom: 0;
+        }
+        .en-name {
+          position: absolute;
+          font-size: 26pt;
+          letter-spacing: 0.2vw;
+          transform: translateX(0);
+          margin-bottom: 0;
+          left: 0;
+          bottom: 3.5vw;
+          margin-left: -13vw;
+        }
+        .small-title {
+          height: 100%;
+          flex: 0;
+          .t {
+            width: 4vw;
+            height: 4vw;
+            padding: 1vw;
+          }
+          &:after {
+            content: "";
+            height: 100%;
+            width: 1px;
+            background: #262626;
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            z-index: 0;
+          }
+        }
+      }
+      .text {
+        padding-left: 0%;
+        width: 100%;
+        letter-spacing: 0.2vw;
+        font-size: 10pt;
+        margin-bottom: 10vw;
+        padding: 0 10vw;
+        box-sizing: border-box;
+        max-height: 200px;
+        overflow: hidden;
+        transition: all .3s;
+        &.show {
+          max-height: 9999px;
+        }
+      }
+      .text-more {
+        position: relative;
+        display: flex;
+        font-weight: bold;
+        font-size: 10pt;
+        align-items: center;
+        padding: 20vw 10vw 10vw 10vw;
+        margin-top: -25vw;
+        background: rgb(255, 255, 255);
+        background: linear-gradient(
+          0deg,
+          rgba(255, 255, 255, 0) 0%,
+          rgba(255, 255, 255, 1) 50%,
+          rgba(255, 255, 255, 0.7) 70%,
+          rgba(255, 255, 255, 0) 100%
+        );
+        transition: all .3s;
+        img {
+          height: 9pt;
+          margin-left: 6pt;
+        }
+        &.hide {
+          display: none;
+        }
+        &:hover {
+          opacity: .7;
+        }
+      }
+      &::after {
+        content: "";
+        width: 100vw;
+        height: 1px;
+        background: #262626;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+    }
+
+    .team {
+      max-width: 80vw;
+      margin: 0 auto;
+      padding-bottom: 0;
+      flex-direction: column;
+      .avatar {
+        width: 40%;
+        margin: 0;
+        margin-bottom: 3vw;
+      }
+      .info {
+        .info-item {
+          margin-bottom: 20vw;
+          .title {
+            font-size: 26pt;
+            letter-spacing: 0.2vw;
+            margin-bottom: 7vw;
+          }
+          .in {
+            margin: 0;
+            padding: 0;
+            border-left: 0;
+            .name {
+              font-size: 12pt;
+            }
+            .text {
+              font-size: 10pt;
+              text-align: justify;
+            }
+          }
+        }
+        &:after {
+          content: "";
+        }
+      }
+      &::after {
+        content: "";
+        width: 100vw;
+        height: 1px;
+        background: #262626;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+    }
+
+    .shop {
+      width: 80vw;
+      margin: 10vw auto;
+      img {
+        &.title {
+          margin-bottom: 7vw;
+        }
+        &.logo {
+          width: 110%;
+          margin-left: -5%;
+        }
+      }
+    }
+  }
+}
 </style>
 <script>
 export default {
   data() {
     return {
+      intro_text_show: false,
       teams: [
         {
           title: "Art Director",
           tag: "藝術總監",
           name: "林昆穎",
-          content: `
+          content: !this.$device.isMobile
+            ? `
             1980 ⽣於花蓮台灣。<br/>
             豪華朗機⼯共同創辦⼈，華麗邏輯有限公司創意總監。<br/>
             以跨域藝術家、⽂化導演與策展⼈身份，<br/>
@@ -262,11 +466,22 @@ export default {
             參與<b>臺北世界⼤學運動會⽂</b>化節⽬導演群與聖⽕台裝置藝術、<br/>
             <b>台中國際花卉博覽會-聆聽花開的聲⾳</b>聲光機導演、<br/>
             <b>臺南藝術節-府城流⽔席</b>總策展人。 
-            `,
+            `
+            : `1980 ⽣於花蓮台灣。
+            豪華朗機⼯共同創辦⼈，華麗邏輯有限公司創意總監。
+            以跨域藝術家、⽂化導演與策展⼈身份，
+            專注於當代議題論述、創意組織營運、藝術產業連結與科技表演編導當中，
+            積極實踐社會共創的各種跨域呈現。
+            擔任<b>2021臺灣⽂博會</b>總策展⼈、
+            <b>2020-2021 臺北⽩晝之夜</b>藝術總監，
+            參與<b>臺北世界⼤學運動會⽂</b>化節⽬導演群與聖⽕台裝置藝術、
+            <b>台中國際花卉博覽會-聆聽花開的聲⾳</b>聲光機導演、
+            <b>臺南藝術節-府城流⽔席</b>總策展人。 `,
         },
         {
           title: "Executive Team",
           tag: "執行團隊",
+          name: !this.$device.isMobile ? "" : "林昆穎",
           content: `
             表演藝術策畫統籌／軟硬倍事<br/>
             視覺統籌／三頁文<br/>
